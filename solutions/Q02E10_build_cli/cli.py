d5f80299007e6de5a0b261d28ee9c858693e8b67
Thomas Marquis <thomas.marquis314@gmail.com>
2022-06-15 06:49:42 +0200

    WIP cli

--- solutions/Q02E10_build_cli/cli.py
+++ solutions/Q02E10_build_cli/cli.py
@@ -1,4 +1,25 @@
+import argparse
 import random
 
 
+def read_recipes():
+    pass
 
+
+def filter_recipes():
+    pass
+
+
+def select_recipes():
+    pass
+
+
+def build_menu():
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build your weekly menu')
+    parser.add_argument('start')
+    
+    args = parser.parse_args()
