52b299a2a0deb5507d650d0dda2985357ca2cace
Thomas Marquis <thomas.marquis314@gmail.com>
2022-06-17 09:12:10 +0200

    80 %

--- subjects/Q02E08_superflex_functions/README.md
+++ subjects/Q02E08_superflex_functions/README.md
@@ -1,6 +1,32 @@
-# Sort a list
+# Hyper flexible function
 
 ## Instructions
+
+Create or reuse file `recipes.py` and add a function `create_recipe_v2`. This function take 4 inputs:
+
+* `title`: the recipe title as a string
+* `persons`: the number of persons, as int
+* `ingredients`: the list of the ingredients
+* `tags`: some optional tags as key/value pairs
+
+It should return a dictionary:
+
+```python
+{
+    'title': 'Tarte aux figues',
+    'persons': 4,
+    'ingredients': ['pâte feuilleté', 'figues'],
+    'tags': {'type': 'dessert', 'vegan': True},
+}
+```
+
+Unlike previous `create_recipe` function, the ingredient list and tags should not be provided to the function as a list or a dictionary. The function can ba callable like this:
+
+```python
+create_recipe_v2('Tarte aux figues', 4, 'pâte feuilleté', 'figues', type='dessert', vegan=True)
+```
+
+This function should perform same verifications as `create_recipe` (title not too long, not too many persons and not empty ingredient list)
 
 
 ## Usage
@@ -8,18 +34,23 @@
 Here is a possible `test.py` to test your functions:
 
 ```python
-import read_recipes
+import recipes
 
 if __name__ == '__main__':
-    print()
+    print(recipes.create_recipe_v2('Tarte aux figues', 4, 'pâte feuilleté', 'figues', type='dessert', vegan=True))
 ```
 
 ```bash
 $ python test.py
-
+{
+    'title': 'Tarte aux figues',
+    'persons': 4,
+    'ingredients': ['pâte feuilleté', 'figues'],
+    'tags': {'type': 'dessert', 'vegan': True},
+}
 ```
 
 
 ## Notions
 
-* []()
+* [args and kwargs](https://realpython.com/python-kwargs-and-args/)
--- tests/Q02E08_superflex_functions/test_recipes.py
+++ tests/Q02E08_superflex_functions/test_recipes.py
@@ -1,9 +1,31 @@
-import pytest
+import inspect
 import re
+
+import pytest
 
 import recipes
 
 ingredients = ['Pate à pizza', 'Frourne d\'Ambert', 'Comté', 'Emental', 'Mozzarella']
+
+
+def test_create_recipe_v2_should_has_ingredients_args():
+    params = inspect.signature(recipes.create_recipe_v2).parameters
+    ingredients_param = params.get('ingredients')
+    if not ingredients_param:
+        pytest.fail('The function should have ingredients parameter')
+    param_kind = ingredients_param.kind
+    expected_king = inspect._ParameterKind.VAR_POSITIONAL
+    assert param_kind == expected_king, 'ingredients parameter should use unpacking operator *'
+
+
+def test_create_recipe_v2_should_has_tags_kwargs():
+    params = inspect.signature(recipes.create_recipe_v2).parameters
+    tags_param = params.get('tags')
+    if not tags_param:
+        pytest.fail('The function should have ingredients parameter')
+    param_kind = tags_param.kind
+    expected_king = inspect._ParameterKind.VAR_KEYWORD
+    assert param_kind == expected_king, 'tags parameter should use unpacking operator **'
 
 
 def test_create_recipe_v2():
@@ -19,15 +41,24 @@ def test_create_recipe_v2_should_raise_value_error_when_empty_ingredients():
 
 
 def test_create_recipe_v2_should_raise_value_error_when_title_too_long():
-    with pytest.raises(ValueError, match=re.compile('^title is too long$', flags=re.I)):
-        recipes.create_recipe_v2('x'*151, 12, *ingredients)
+    try:
+        with pytest.raises(ValueError, match=re.compile('^title is too long$', flags=re.I)):
+            recipes.create_recipe_v2('x'*151, 12, *ingredients)
+    except TypeError:
+        pass
 
 
 def test_create_recipe_v2_should_raise_value_error_when_too_many_persons():
-    with pytest.raises(ValueError, match=re.compile('^invalid persons number$', flags=re.I)):
-        recipes.create_recipe_v2('tacos à la grec', 51, *ingredients)
+    try:
+        with pytest.raises(ValueError, match=re.compile('^invalid persons number$', flags=re.I)):
+            recipes.create_recipe_v2('tacos à la grec', 51, *ingredients)
+    except TypeError:
+        pass
 
 
 def test_create_recipe_v2_should_raise_value_error_when_zero_persons():
-    with pytest.raises(ValueError, match=re.compile('^invalid persons number$', flags=re.I)):
-        recipes.create_recipe_v2('tacos à la grec', 0, *ingredients)
+    try:
+        with pytest.raises(ValueError, match=re.compile('^invalid persons number$', flags=re.I)):
+            recipes.create_recipe_v2('tacos à la grec', 0, *ingredients)
+    except TypeError:
+        pass
