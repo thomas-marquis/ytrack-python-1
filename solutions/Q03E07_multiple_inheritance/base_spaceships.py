680c0fef748cdfa14eeced55e2d0c2e47a2c4275
Thomas Marquis <thomas.marquis314@gmail.com>
2022-07-13 06:54:00 +0200

    add exo WIP

---
+++ solutions/Q03E07_multiple_inheritance/base_spaceships.py
@@ -0,0 +1,27 @@
+class Spaceship:
+    is_alive: bool = True
+    
+    def __init__(self, attack: int, defense: int):
+        self.attack = attack
+        self.defense = defense
+        
+    def take_damages(self, damages: int):
+        """Take damages and decrease the ship defense amount. If ship defense reach 0, ship is dead."""
+        if damages < 0:
+            raise ValueError('damages value should be a positive integer')
+        self.defense -= damages
+        if self.defense <= 0:
+            self.defense = 0
+            self.is_alive = False
+        
+    def fire_on(self, other_ship: 'Spaceship') -> 'Spaceship':
+        other_ship.take_damages(self.attack)
+        return other_ship
+    
+
+class Battleship(Spaceship):
+    pass
+
+
+class Fighter(Spaceship):
+    pass
---
+++ solutions/Q03E07_multiple_inheritance/spaceships.py
@@ -0,0 +1,51 @@
+from base_spaceships import Battleship, Fighter, Spaceship
+
+
+class BattleshipKiller:
+    bonus = 2
+    
+    def fire_on(self, other_ship: Spaceship) -> Spaceship:
+        attack = self.attack
+        if isinstance(other_ship, Battleship):    
+            attack *= self.bonus
+            
+        other_ship.take_damages(attack)
+        return other_ship
+
+
+class FighterKiller:
+    bonus = 2
+    
+    def fire_on(self, other_ship: Spaceship) -> Spaceship:
+        attack = self.attack
+        if isinstance(other_ship, Fighter):
+            attack *= self.bonus
+            
+        other_ship.take_damages(attack)
+        return other_ship
+
+
+class Interceptor(FighterKiller, Fighter):
+    def __init__(self):
+        super().__init__(attack=180, defense=1000)
+        
+
+class Bomber(BattleshipKiller, Fighter):
+    def __init__(self):
+        super().__init__(attack=150, defense=2000)
+    
+    
+class Cruiser(Battleship):
+    def __init__(self):
+        super().__init__(attack=800, defense=3000)
+        
+        
+class Frigate(FighterKiller, Battleship):
+    def __init__(self):
+        super().__init__(attack=500, defense=2500)
+        
+        
+class Destroyer(BattleshipKiller, Battleship):
+    def __init__(self):
+        super().__init__(attack=650, defense=5000)
+        
